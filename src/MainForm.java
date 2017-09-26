eb399d6c27d027402f8fe23ae06c441c5169a000
sj96 <sj.dev.96@gmail.com>
2017-09-26 23:06:01 +0700

    add dangkythamgia

--- src/MainForm.java
+++ src/MainForm.java
@@ -26,9 +26,11 @@ public class MainForm extends javax.swing.JFrame {
         btnQuanLySinhVien = new javax.swing.JButton();
         btnThongKe = new javax.swing.JButton();
         Body = new javax.swing.JPanel();
+        dangkythamgia2 = new DiemDanh.Dangkythamgia();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(255, 255, 255));
+        setMinimumSize(new java.awt.Dimension(996, 662));
 
         Header.setBackground(new java.awt.Color(0, 102, 204));
 
@@ -52,7 +54,7 @@ public class MainForm extends javax.swing.JFrame {
                 .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 111, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(93, 93, 93)
                 .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 433, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 186, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 190, Short.MAX_VALUE)
                 .addComponent(btnDangNhap)
                 .addContainerGap())
         );
@@ -125,8 +127,9 @@ public class MainForm extends javax.swing.JFrame {
 
         Body.setBackground(new java.awt.Color(255, 255, 255));
         Body.setMinimumSize(new java.awt.Dimension(996, 511));
-        Body.setPreferredSize(new java.awt.Dimension(0, 425));
+        Body.setPreferredSize(new java.awt.Dimension(996, 511));
         Body.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        Body.add(dangkythamgia2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -143,7 +146,7 @@ public class MainForm extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(Menu, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(Body, javax.swing.GroupLayout.DEFAULT_SIZE, 511, Short.MAX_VALUE))
+                .addComponent(Body, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
@@ -211,6 +214,7 @@ public class MainForm extends javax.swing.JFrame {
     private javax.swing.JButton btnQuanLySinhVien;
     private javax.swing.JButton btnQunLiSuKien;
     private javax.swing.JButton btnThongKe;
+    private DiemDanh.Dangkythamgia dangkythamgia2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel3;
     // End of variables declaration//GEN-END:variables
